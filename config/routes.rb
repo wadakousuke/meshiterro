49d7a15cb73b06b39bfbf8fe3a33733854dcd8be
wadakousuke <100855797+wadakousuke@users.noreply.github.com>
2022-03-19 12:46:22 +0000

    [Finish 13章　削除機能の追加]

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as:"about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :post_images, only: [:new, :index, :show, :create]
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
 end
